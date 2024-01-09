da0b7b9f02e45fb35fd0d581cd6eca16b29aef97
Patrick Gross <bears_lair@protonmail.com>
2024-01-09 15:23:41 -0800

    connected buttons to the DOM

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ let firstNum = 0;
 let operator;
 let secondNum = 0;
 
+const sevenBtn = document.querySelector("btn seven");
+const eightBtn = document.querySelector("btn eight");
+const nineBtn = document.querySelector("btn nine");
+const addBtn = document.querySelector("btn add");
+const fourBtn = document.querySelector("btn four");
+const fiveBtn = document.querySelector("btn five");
+const sixBtn = document.querySelector("btn six");
+const subtractBtn = document.querySelector("btn subtract");
+const oneBtn = document.querySelector("btn one");
+const twoBtn = document.querySelector("btn two");
+const threeBtn = document.querySelector("btn three");
+const multiplyBtn = document.querySelector("btn multiply");
+const clearBtn = document.querySelector("btn clear");
+const zeroBtn = document.querySelector("btn zero");
+const equalsBtn = document.querySelector("btn equals");
+const divideBtn = document.querySelector("btn divide");
+
 function add(firstNum, secondNum) {
   return firstNum + secondNum;
 }
